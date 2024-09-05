b94a165f10559f14678b1cbcba3b0bd3db66aa79
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-09-05 13:01:45 -0300

    chore: Update eslint and next.config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+    <main className="flex justify-center items-center p-12 bg-orange-400 sm:pb-12 lg:pb-12 xl:pb-12">
+      <Image
+        src="https://github.com/Mateus8741.png"
+        alt="Mateus Tavares"
+        width={120}
+        height={120}
+        className="rounded-full"
+      />
+
+      <h1>Mateus Tavares</h1>
     </main>
-  );
+  )
 }
