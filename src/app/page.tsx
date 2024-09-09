822ff3461a2ceb0449dc9d0ba7221bee276713ed
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-09-09 18:58:34 -0300

    chore: Update dependencies and remove Clerk configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,9 @@
-import Image from 'next/image'
-
 export default function Home() {
   return (
     <main className="flex justify-center items-center p-12 bg-orange-400 sm:pb-12 lg:pb-12 xl:pb-12">
-      <Image
-        src="https://github.com/Mateus8741.png"
-        alt="Mateus Tavares"
-        width={120}
-        height={120}
-        className="rounded-full"
-      />
-
       <h1>Mateus Tavares</h1>
+
+      <h2>Login screen</h2>
     </main>
   )
 }
