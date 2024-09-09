4e025687bb25d6f86595f044b405e324629774d3
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-09-09 19:04:59 -0300

    Refactor: Remove Clerk configuration and update layout styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex justify-center items-center p-12 bg-orange-400 sm:pb-12 lg:pb-12 xl:pb-12">
+    <main className="flex justify-center h-screen items-center p-12 bg-orange-400">
       <h1>Mateus Tavares</h1>
 
       <h2>Login screen</h2>
