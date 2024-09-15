7ec165619cb858150afe5914688000672321358d
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-09-15 15:15:19 -0300

    Refactor: Update LinkCard component to use white background instead of green

--- src/components/linkCard.tsx
+++ src/components/linkCard.tsx
@@ -6,7 +6,7 @@ interface LinkCardProps {
 
 export function LinkCard({ link }: LinkCardProps) {
   return (
-    <div className="flex items-center bg-green-100 p-4 gap-1 rounded-xl shadow-lg">
+    <div className="flex items-center bg-white p-4 gap-1 rounded-xl shadow-lg">
       <FlameIcon className="size-8 text-orange-500" />
       <h1 className="text-xl font-bold text-black">Seu My.Tree está no ar: </h1>
       <span className="text-green-500 underline">{link}</span>
