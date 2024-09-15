ba5d8dc8db4788da176311be640f6315441247dd
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-09-15 01:31:44 -0300

    Refactor: Add Link component to AdjacentLinks

--- src/components/adjacentLinks.tsx
+++ src/components/adjacentLinks.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 interface AdjacentLinksProps {
   link: string
   tittle: string
@@ -6,12 +8,13 @@ interface AdjacentLinksProps {
 export function AdjacentLinks({ link, tittle }: AdjacentLinksProps) {
   return (
     <div className="flex items-center gap-1 mt-8">
-      <a
+      <Link
         href={link}
+        passHref
         className="relative text-green-600 after:block after:absolute after:left-0 after-bottom-0 after:w-full after:h-[2px] after:bg-green-600 after:scale-x-0 after:origin-left after:transition-transform after:duration-300 hover:after:scale-x-100"
       >
         {tittle}
-      </a>
+      </Link>
     </div>
   )
 }
