717d6948518376bd6161383e4e33fa3fbd079ab7
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-09-15 03:03:24 -0300

    Refactor: Update Logo component to use variant and size props

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,8 +1,29 @@
+import { cn } from '@/lib/utils'
+import { cva, type VariantProps } from 'class-variance-authority'
 import { TreePalm } from 'lucide-react'
 
-export function Logo() {
+const logoVariants = cva('flex items-center font-bold', {
+  variants: {
+    variant: {
+      default: 'text-green-600',
+    },
+    size: {
+      sm: 'text-4xl',
+      md: 'text-7xl',
+      lg: 'text-9xl',
+    },
+  },
+  defaultVariants: {
+    variant: 'default',
+    size: 'md',
+  },
+})
+
+interface LogoProps extends VariantProps<typeof logoVariants> {}
+
+export function Logo({ variant, size }: LogoProps) {
   return (
-    <div className="flex font-bold text-7xl text-green-600">
+    <div className={cn(logoVariants({ variant, size }))}>
       <span className="text-gray-900">My.</span>
       <span>Tree</span>
       <TreePalm size={32} />
