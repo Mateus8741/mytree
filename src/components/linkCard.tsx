9c09a03af63c29c19aef5795cbd93afe801bf116
Mateus  Tavares <mateus.sousa@soterotech.com.br>
2024-09-18 12:59:51 -0300

    Refactor: Update LinkCard component to include clickable link with custom URL

--- src/components/linkCard.tsx
+++ src/components/linkCard.tsx
@@ -1,15 +1,22 @@
 import { FlameIcon } from 'lucide-react'
+import Link from 'next/link'
 
 interface LinkCardProps {
   link: string
 }
 
 export function LinkCard({ link }: LinkCardProps) {
+  const href = `/MyLink/${link}`
   return (
     <div className="flex items-center bg-white p-4 gap-1 rounded-xl shadow-lg">
       <FlameIcon className="size-8 text-orange-500" />
       <h1 className="text-xl font-bold text-black">Seu My.Tree está no ar: </h1>
-      <span className="text-green-500 underline">{link}</span>
+      <Link
+        href={href}
+        className="text-green-500 underline hover:text-green-700"
+      >
+        <span>my.tree/{link}</span>
+      </Link>
     </div>
   )
 }
